579fb962505d66bcc271eab6f70a3840b4f410eb
aharoni111 <aharoni111@gmail.com>
2022-08-23 16:52:51 +0300

    aa

--- pages/api/getbook.js
+++ pages/api/getbook.js
@@ -1,12 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const fs = require('fs')
+const moment = require('moment');
 
 export default function handler(req, res) {
+    
     if (req.method === 'GET') {
       // Process a POST request
 
     
       const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    
+      fs.appendFile('./wLog.txt', ip + '  ' + moment().format("DD/MM/YYYY h:mm") + '\r\n' , err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
       console.log(ip)
       console.log(req.url)
       res.send(ip)
